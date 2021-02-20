e7749f64b741267d5627f17d1eda0d7d59568a35
mjk1252 <mjk.visser@gmail.com>
2021-02-20 15:40:32 +0200

    Complete Article API

--- app.js
+++ app.js
@@ -31,45 +31,106 @@ app.use(express.static("public"));
 
 //TODO
 
+
+// Chain Routes for all //////////////////////////////////////////////
 app.route("/articles")
 
-.get((req, res) => {
-    Article.find({}, (err, foundArticles) => {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        })
     })
-})
 
 
-.post((req, res) => {
+    .post((req, res) => {
 
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("Success!");
+            } else {
+                res.send(err);
+            }
+        })
+    })
+
+
+    .delete(function (req, res) {
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Deleted all articles");
+            } else {
+                res.send(err);
+            }
+        })
     });
 
-    newArticle.save((err) => {
-        if (!err) {
-            res.send("Success!");
-        } else {
-            res.send(err);
-        }
-    })
-})
 
+// Chain Routes for one //////////////////////////////////////////////
+app.route("/articles/:articleTitle")
 
-.delete(function (req, res) {
-    Article.deleteMany((err) => {
-        if (!err) {
-            res.send("Deleted all articles");
-        } else {
-            res.send(err);
-        }
+    .get((req, res) => {
+        const articleTitle = req.params.articleTitle;
+
+        Article.findOne({
+            title: articleTitle
+        }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send("Not Such Article found")
+            }
+        });
     })
-});
+
+    .put((req, res) => {
+        Article.findOneAndUpdate(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+            {upsert: true},
+            (err) => {
+            if (!err) {
+                res.send("Updated Article")
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    
+    .patch((req, res) => {
+        Article.findOneAndUpdate(
+            {title: req.params.articleTitle},
+            {$set: req.body},
+            {upsert: false},
+            (err) => {
+            if (!err) {
+                res.send("Updated Article")
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    
+    
+    .delete((req, res) => {
+        Article.deleteOne({title: req.params.articleTitle}, (err) => {
+            if(!err){
+                res.send("Sucessfully Deleted")
+            }
+            else{
+                res.send("Could not delete required article or article couldnt be find")
+            }
+        })
+    });
+
 
 
 
